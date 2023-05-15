3cf003462effc4272a135cd291fa4bb43a307e34
João Vitor <joaoezeki@gmail.com>
2023-05-15 15:56:32 -0300

    Update CardProdutos.tsx

--- app/src/components/CardProdutos.tsx
+++ app/src/components/CardProdutos.tsx
@@ -2,11 +2,12 @@
 
 export const CardPordutos = () => {
   return (
-    <div className="bg-red-500 h-80 w-56 mx-5 my-5">
+    <div className="bg-red-500 h-80 w-56 mx-5 my-5 rounded">
       <div className="bg-blue-600 h-3/4"></div>
 
-      <div className="text-center">
-        <p className="my-1">Nome</p>
+      <div className="text-center h-20 flex flex-col justify-between">
+        <p className="mt-2">Nome</p>
+        <p className="mb-2">Preço</p>
       </div>
     </div>
   )
